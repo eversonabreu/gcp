9d9c1cf8b6673edfdd4d29b08412867fcaba5fd7
IZABELA MARIA ALVES SOARES <izabela.soares@alunos.sc.senac.br>
2021-10-29 22:04:14 -0300

    Adicionado ConcursoEntity

--- Senac.GCP/Senac.GCP.Domain/Entities/ConcursoEntity.cs
+++ Senac.GCP/Senac.GCP.Domain/Entities/ConcursoEntity.cs
@@ -1,37 +1,43 @@
 ﻿using Senac.GCP.Domain.Attributes;
 using Senac.GCP.Domain.Entities.Base;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Senac.GCP.Domain.Entities
 {
-    public sealed class InstituicaoEntity : Entity
+    public sealed class ConcursoEntity : Entity
     {
-        public string Nome { get; set; }
+        public int Codigo { get; set; }
 
-        public string CNPJ { get; set; }
+        public string Descricao { get; set; }
 
-        public string EnderecoRua { get; set; }
+        public DateTime DataInicioInscricao { get; set; }
 
-        public string EnderecoBairro { get; set; }
+        public DateTime DataFinalInscricao { get; set; }
 
-        public string EnderecoNumero { get; set; }
+        public int IdInstituicaoSolicitante { get; set; }
 
-        public string EnderecoComplemento { get; set; }
+        public int IdInstituicaoOrganizadora { get; set; }
 
-        public string EnderecoCEP { get; set; }
+        public DateTime PrazoFinalIsencaoValorInscricao { get; set; }
+
+        public Decimal ValorInscricao { get; set; }
 
         public bool Ativo { get; set; }
 
+        public int QuantidadeVagas { get; set; }
+
+        public int PercentualQuantidadeVagasAmplaConcorrencia { get; set; }
+
         public string Observacoes { get; set; }
 
-        public long IdMunicipio { get; set; }
-
-        public string Email { get; set; }
-
-        public string Telefone { get; set; }
 
         [NotMapped]
-        [Dependency(NameForeignKey = nameof(IdMunicipio))]
-        public MunicipioEntity Municipio { get; set; }
+        [Dependency(NameForeignKey = nameof(IdInstituicaoOrganizadora))]
+        public InstituicaoEntity InstituicaoOrganizadora { get; set; }
+
+        [NotMapped]
+        [Dependency(NameForeignKey = nameof(IdInstituicaoSolicitante))]
+        public InstituicaoEntity InstituicaoSolicitante { get; set; }
     }
 }
