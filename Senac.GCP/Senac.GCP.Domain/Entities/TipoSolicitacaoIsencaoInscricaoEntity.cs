ac69b51f7b439e505def073025efb576b8ed8453
rodrigo leonardo <rodrigoleonardo81@gmail.com>
2021-10-31 20:23:33 -0300

    arrumando o nome da classe tipoSolicitacaoIsencaoInscricao

--- Senac.GCP/Senac.GCP.Domain/Entities/TipoSolicitacaoIsencaoInscricaoEntity.cs
+++ Senac.GCP/Senac.GCP.Domain/Entities/TipoSolicitacaoIsencaoInscricaoEntity.cs
@@ -2,7 +2,7 @@
 
 namespace Senac.GCP.Domain.Entities
 {
-    public sealed class TipoSolicitacaoIsencaoInscricao : Entity
+    public sealed class TipoSolicitacaoIsencaoInscricaoEntity : Entity
     {
         public string Codigo { get; set; }
         public string Descricao { get; set; }
