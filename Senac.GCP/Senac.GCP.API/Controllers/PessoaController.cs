0fc7e930987d9dfeed3f7173e0476b62a82eabfb
Mateus Petri <mateuspetri_2012@hotmail.com>
2021-10-31 21:59:21 -0300

    Criado estrutura API da tabela Pessoa

---
+++ Senac.GCP/Senac.GCP.API/Controllers/PessoaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senac.GCP.API.Controllers.Base;
+using Senac.GCP.API.Models;
+using Senac.GCP.Domain.Entities;
+using Senac.GCP.Domain.Services.Interfaces;
+
+namespace Senac.GCP.API.Controllers
+{
+    [Route("pessoa")]
+    public sealed class PessoaController : Controller<PessoaModel, PessoaEntity>
+    {
+        public PessoaController(IPessoaService pessoaService) : base(pessoaService)
+        {
+        
+        }
+
+    }
+}
---
+++ Senac.GCP/Senac.GCP.Domain/Entities/PessoaEntity.cs
@@ -0,0 +1,58 @@
+﻿using Senac.GCP.Domain.Entities.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+
+namespace Senac.GCP.Domain.Entities
+{
+    public sealed class PessoaEntity : Entity
+    {
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+        public DateTime DataEmissaoRG { get; set; }
+
+        public string OrgaoEmissorRG { get; set; }
+
+        public char Genero { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public bool PCD { get; set; }
+
+        public long IdNacionalidade { get; set; }
+
+        public long IdClassificacaoDoenca { get; set; }
+
+        public long IdCorRaca { get; set; }
+
+        public string EnderecoRua { get; set; }
+
+        public string EnderecoNumero { get; set; }
+
+        public string EnderecoBairro { get; set; }
+
+        public string EnderecoComplemento { get; set; }
+
+        public string EnderecoCEP { get; set; }
+
+        public long IdMunicipio { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public string ChaveAcesso { get; set; }
+
+        public long IdArquivoFoto { get; set; }
+
+        public long IdNaturalidade { get; set; }
+
+    }
+}
