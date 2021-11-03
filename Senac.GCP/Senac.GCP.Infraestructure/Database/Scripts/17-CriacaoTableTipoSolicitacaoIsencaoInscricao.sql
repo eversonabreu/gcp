c9e4db0377cbf5ca754cdf81cd844704e17c3df5
rodrigo leonardo <rodrigoleonardo81@gmail.com>
2021-11-03 19:07:31 -0300

    ajustado a nomenclatura na tabela TipoSolicitacaoIsencaoInscricao

--- Senac.GCP/Senac.GCP.Infraestructure/Database/Scripts/17-CriacaoTableTipoSolicitacaoIsencaoInscricao.sql
+++ Senac.GCP/Senac.GCP.Infraestructure/Database/Scripts/17-CriacaoTableTipoSolicitacaoIsencaoInscricao.sql
@@ -1,7 +1,7 @@
 create table TipoSolicitacaoIsencaoInscricao(
-	id bigint not null identity(1,1),
-	codigo varchar(50) not null,
-	descricao varchar (255) not null,
+	Id bigint not null identity(1,1),
+	Codigo varchar(50) not null,
+	Descricao varchar (255) not null,
 	constraint PkTipoSolicitacaoIsencaoInscricao primary key (id),
 	constraint UkTipoSolicitacaoIsencaoInscricao unique (codigo)
 );
