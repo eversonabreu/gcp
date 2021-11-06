aa4c8586d25565571642deb6106898a37a07f0b8
Amanda Hirata <amanda.hirata99@gmail.com>
2021-11-05 23:27:53 -0300

    Atualização InscricoesModel

--- Senac.GCP/Senac.GCP.API/Models/InscricoesModel.cs
+++ Senac.GCP/Senac.GCP.API/Models/InscricoesModel.cs
@@ -1,4 +1,5 @@
 ﻿using Senac.GCP.API.Models.Base;
+using Senac.GCP.Domain.Attributes;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -6,23 +7,30 @@ namespace Senac.GCP.API.Models
 {
     public sealed class Inscricoes : Model
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "A 'Pessoa' deve ser informada obrigatoriamente.")]
+        [StringLength(maximumLength: 255, ErrorMessage = "O tamanho do campo 'Pessoa' não pode superar 255 caractéres.")]
         public long IdPessoa { get; set; }
 
+        [Required(ErrorMessage = "O Campo 'IdConcurso' Deve Ser Informado Obrigatóriamente.")]
+        [DateOnly]
         public long IdConcurso { get; set; }
 
+        [Required(ErrorMessage = "O Campo 'Data da Inscricao' Deve Ser Informado Obrigatóriamente.")]        
+        [DateOnly]
         public DateTime DataInscricao { get; set; }
 
+        [Required(ErrorMessage = "O Campo 'Número da inscrição' Deve Ser Informado Obrigatóriamente.")]
         public string NumeroInscricao { get; set; }
 
         public int Situacao { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "A 'Pessoa' deve ser informada obrigatoriamente.")]
+        [StringLength(maximumLength: 255, ErrorMessage = "O campo 'Participar como cotista' deve ser informado")]
         public bool ParticiparComoCotista { get; set; }
 
         public string MotivoRecusaInscricao { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O 'Nome' não foi preenchido")]
-        [StringLength(maximumLength: 200, ErrorMessage = "O 'Nome' aceita no máx 200 caracteres")]
-
+        [Required(ErrorMessage = "O Campo 'Data da Recusa' Deve Ser Informado Obrigatóriamente.")]
         public DateTime DataRecusaInscricao { get; set; }
 
         public PessoaModel Pessoa { get; set; }
