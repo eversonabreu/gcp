88d524b065148f34a946a086074366b85b589c33
Amanda Hirata <amanda.hirata99@gmail.com>
2021-11-05 22:54:07 -0300

    Código InscricaoMode VS

---
+++ Senac.GCP/Senac.GCP.API/Models/InscricoesModel.cs
@@ -0,0 +1,34 @@
+﻿using Senac.GCP.API.Models.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Senac.GCP.API.Models
+{
+    public sealed class Inscricoes : Model
+    {
+        public long IdPessoa { get; set; }
+
+        public long IdConcurso { get; set; }
+
+        public DateTime DataInscricao { get; set; }
+
+        public string NumeroInscricao { get; set; }
+
+        public int Situacao { get; set; }
+
+        public bool ParticiparComoCotista { get; set; }
+
+        public string MotivoRecusaInscricao { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O 'Nome' não foi preenchido")]
+        [StringLength(maximumLength: 200, ErrorMessage = "O 'Nome' aceita no máx 200 caracteres")]
+
+        public DateTime DataRecusaInscricao { get; set; }
+
+        public PessoaModel Pessoa { get; set; }
+       
+        public ConcursoModel Concurso { get; set; }
+
+
+    }
+}
--- Senac.GCP/Senac.GCP.Domain/Entities/InscricoesEntity.cs
+++ Senac.GCP/Senac.GCP.Domain/Entities/InscricoesEntity.cs
@@ -1,34 +1,34 @@
-﻿//using Senac.GCP.Domain.Attributes;
-//using Senac.GCP.Domain.Entities.Base;
-//using System;
-//using System.ComponentModel.DataAnnotations.Schema;
+﻿using Senac.GCP.Domain.Attributes;
+using Senac.GCP.Domain.Entities.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
-//namespace Senac.GCP.Domain.Entities
-//{
-//    public sealed class InscricoesEntity : Entity
-//    {
-//        public long IdPessoa { get; set; }
+namespace Senac.GCP.Domain.Entities
+{
+    public sealed class InscricoesEntity : Entity
+    {
+        public long IdPessoa { get; set; }
 
-//        public long IdConcurso { get; set; }
+        public long IdConcurso { get; set; }
 
-//        public DateTime DataInscricao { get; set; }
+        public DateTime DataInscricao { get; set; }
 
-//        public string NumeroInscricao { get; set; }
+        public string NumeroInscricao { get; set; }
 
-//        public int Situacao { get; set; }
+        public int Situacao { get; set; }
 
-//        public bool ParticiparComoCotista { get; set; }
+        public bool ParticiparComoCotista { get; set; }
 
-//        public string MotivoRecusaInscricao { get; set; }
+        public string MotivoRecusaInscricao { get; set; }
 
-//        public DateTime DataRecusaInscricao { get; set; }
+        public DateTime DataRecusaInscricao { get; set; }
 
-//        [NotMapped]
-//        [Dependency(NameForeignKey = nameof(IdPessoa))]
-//        public PessoaEntity Pessoa { get; set; }
+        [NotMapped]
+        [Dependency(NameForeignKey = nameof(IdPessoa))]
+        public PessoaEntity Pessoa { get; set; }
 
-//        [NotMapped]
-//        [Dependency(NameForeignKey = nameof(IdConcurso))]
-//        public ConcursoEntity Concurso { get; set; }
-//    }
-//}
+        [NotMapped]
+        [Dependency(NameForeignKey = nameof(IdConcurso))]
+        public ConcursoEntity Concurso { get; set; }
+    }
+}
