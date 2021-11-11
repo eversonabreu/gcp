6aca09a0edf8c3d6815f78f9a6e315f62ccacf18
ALEXANDRO DEBASTIANI AGUIAR BRANDT <alexandro.brandt@alunos.sc.senac.br>
2021-11-10 22:02:37 -0300

    Treze passos tabela ConcursoFaseCargo

---
+++ Senac.GCP/Senac.GCP.API/Controllers/ConcursoFaseCargoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senac.GCP.API.Controllers.Base;
+using Senac.GCP.API.Models;
+using Senac.GCP.Domain.Entities;
+using Senac.GCP.Domain.Services.Interfaces;
+
+namespace Senac.GCP.API.Controllers
+{
+    [Route("concurso-fase-cargo")]
+    public sealed class ConcursoFaseCargoController : Controller<ConcursoCargoModel, ConcursoFaseCargoEntity>
+    {
+        public ConcursoFaseCargoController(IConcursoFaseCargoService ConcursoFaseCargoService) : base(ConcursoFaseCargoService)
+        {
+        }
+    }
+}
---
+++ Senac.GCP/Senac.GCP.API/Models/ConcursoFaseCargoModel.cs
@@ -0,0 +1,18 @@
+﻿using Senac.GCP.API.Models.Base;
+using Senac.GCP.Domain.Attributes;
+using Senac.GCP.Domain.Enums;
+using Senac.GCP.Domain.Exceptions;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Senac.GCP.API.Models
+{
+    [Constraint(Name = "FkIdConcursoCargo", ErrorMessage = "O 'Id Concurso Cargo' não é válido ou não foi atribuído corretamente")]
+    [Constraint(Name = "FkIdConcursoFase", ErrorMessage = "O 'Id Concurso Fase' não é válido ou não foi atribuído corretamente")]
+    public sealed class ConcursoFaseCargoModel : Model
+    {
+        public long IdConcusoCargo { get; set; }
+
+        public long IdConcursoFase { get; set; }
+    }
+}
