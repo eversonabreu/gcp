a60760bc277505850d3e615ebf7c138811bae220
LUIGI GARCIA MARCHETTI <luigi.marchetti@alunos.sc.senac.br>
2021-11-17 20:55:34 -0300

    ConcursoFasesLocaisSala corrigo

--- Senac.GCP/Senac.GCP.API/Models/ConcursoFasesLocaisSalaModel.cs
+++ Senac.GCP/Senac.GCP.API/Models/ConcursoFasesLocaisSalaModel.cs
@@ -1,6 +1,7 @@
 ﻿using Senac.GCP.API.Models.Base;
 using Senac.GCP.Domain.Attributes;
 using Senac.GCP.Domain.Enums;
+using Senac.GCP.Domain.Exceptions;
 using System.ComponentModel.DataAnnotations;
 
 namespace Senac.GCP.API.Models
@@ -18,5 +19,13 @@ namespace Senac.GCP.API.Models
         public int QuantidadeDeCarteiras { get; set; }
         public int QuantidadeDeCarteirasPcd { get; set; }
 
+        public override void AdditionalValidations()
+        {
+            if (QuantidadeDeCarteiras < 0 || QuantidadeDeCarteirasPcd < 0)
+                throw new BusinessException("O valor inserido em uma das colunas de quantidade é inferior a 0 ");
+
+            if (QuantidadeDeCarteiras == 0 && QuantidadeDeCarteirasPcd == 0)
+                throw new BusinessException("Os valores inseridos não correspondem a uma quantidade de carteiras valida, pois não ha carteiras ");
+        }
     }
 }
