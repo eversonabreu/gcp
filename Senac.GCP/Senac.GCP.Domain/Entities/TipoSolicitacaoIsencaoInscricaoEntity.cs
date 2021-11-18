f415d95d749e09ac544b69aab7db59b857b676a6
LUIGI GARCIA MARCHETTI <luigi.marchetti@alunos.sc.senac.br>
2021-11-17 21:29:33 -0300

    tipo solicitacao isencao inscricao e concurso constraints

--- Senac.GCP/Senac.GCP.Domain/Entities/TipoSolicitacaoIsencaoInscricaoEntity.cs
+++ Senac.GCP/Senac.GCP.Domain/Entities/TipoSolicitacaoIsencaoInscricaoEntity.cs
@@ -1,7 +1,9 @@
-﻿using Senac.GCP.Domain.Entities.Base;
+﻿using Senac.GCP.Domain.Attributes;
+using Senac.GCP.Domain.Entities.Base;
 
 namespace Senac.GCP.Domain.Entities
 {
+    [Constraint(Name = "UkTipoSolicitacaoIsencaoInscricao", ErrorMessage = "O Codigo digitado nao e valido")]
     public sealed class TipoSolicitacaoIsencaoInscricaoEntity : Entity
     {
         public string Codigo { get; set; }
