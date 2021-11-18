1a2fa21baf9a9292aee277299893f95328c2d53f
LUIGI GARCIA MARCHETTI <luigi.marchetti@alunos.sc.senac.br>
2021-11-17 21:47:24 -0300

    constraint municipio

--- Senac.GCP/Senac.GCP.Domain/Entities/EstadoEntity.cs
+++ Senac.GCP/Senac.GCP.Domain/Entities/EstadoEntity.cs
@@ -1,9 +1,8 @@
-﻿using Senac.GCP.Domain.Entities.Base;
-using Xunit;
-
+﻿using Senac.GCP.Domain.Attributes;
+using Senac.GCP.Domain.Entities.Base;
 namespace Senac.GCP.Domain.Entities
 {
-    [Constraint(Name = "UKCodigoCargo", ErrorMessage = "O 'codigo' não é válido ou não foi atribuído corretamente")]
+    [Constraint(Name = "UKCodigoCargo", ErrorMessage = "A 'SiglaUF' não é válido ou não foi atribuído corretamente")]
 
     public sealed class EstadoEntity : Entity
     {
